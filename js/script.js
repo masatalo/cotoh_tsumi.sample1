5338fbf13f5ae0265c24dc6a273936e23b7560c0
masatalo <65925679+masatalo@users.noreply.github.com>
2020-07-12 16:25:30 +0900

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,26 @@
+
+
+(function ($) {
+  $(function () {
+    var $header = $('#top-head');
+    // Nav Fixed
+    $(window).scroll(function () {
+      if ($(window).scrollTop() > 350) {
+        $header.addClass('fixed');
+      } else {
+        $header.removeClass('fixed');
+      }
+    });
+    // Nav Toggle Button
+    $('#nav-toggle').click(function () {
+      $header.toggleClass('open');
+    });
+  });
+})(jQuery);
+
+
+$('.grid').masonry({
+itemSelector: '.grid-item',
+columnWidth: 160,
+gutter: 20
+});
